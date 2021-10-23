4b80722dc2f67bb5a6cbca55a4adf2b4da84e5a7
kakudo <kakudo415@gmail.com>
2021-10-24 00:28:47 +0900

    新規コメント作成フォームを書き直して、ページ遷移に対応

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,32 @@
-const form = document.getElementById("form");
-const button = document.getElementById("button");
+const newTopicFormSubmitButton = document.querySelector("button.new-topic-form-submit-button");
 
-button.addEventListener("click", (event) => {
-  const data = new FormData(form);
-  let obj = {};
-  obj.parent_comment_id = 0;
-  obj.title = data.get("title");
-  obj.text = data.get("text");
-  obj.attribute = Number(data.get("sanpi"));
+newTopicFormSubmitButton.addEventListener("click", () => {
+  const newTopicForm = document.querySelector("form.new-topic-form");
+  let formData = new FormData(newTopicForm);
+  let requestData = {};
+  requestData.parent_comment_id = 0;
+  requestData.title = formData.get("title");
+  requestData.text = formData.get("content");
+  if (formData.get("position") === "agree") {
+    requestData.attribute = 1;
+  }
+  if (formData.get("position") === "neutral") {
+    requestData.attribute = 0;
+  }
+  if (formData.get("position") === "disagree") {
+    requestData.attribute = -1;
+  }
   fetch("/comment", {
     method: "POST",
     headers: {
       "Content-Type": "application/json; charset=UTF-8"
     },
-    body: JSON.stringify(obj)
-  })
-    .then((res) => { return res.text(); })
-    .then((data) => { console.log(data) })
-    .catch((err) => { console.log(err); });
-}, false);
+    body: JSON.stringify(requestData)
+  }).then((response) => {
+    return response.json();
+  }).then((responseJSON) => {
+    location.pathname = `/comment/${responseJSON.comment_id}`;
+  }).catch((error) => {
+    console.error(error);
+  });
+});
